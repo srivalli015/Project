78ff4ac3bd0c18a407f05bc86ebd5bda361e3031
srivalli015 <58562261+srivalli015@users.noreply.github.com>
2019-12-11 17:16:25 +0530

    Add files via upload

---
+++ login.java
@@ -0,0 +1,31 @@
+package loginpage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class login {
+
+	public static void main(String[] args)
+	{
+		System.setProperty("webdriver.gecko.driver", "D:\\$riv@lli\\geckodriver.exe");
+	String bpath = "http://apps.qaplanet.in/qahrm/login.php";
+	//WebDriver driver= new FirefoxDriver();
+	WebDriver driver; 
+	driver = new FirefoxDriver();
+	driver.get(bpath);
+	
+	//initializing user name and password
+	driver.findElement(By.xpath("//input[@name='txtUserName']")).sendKeys("qaplanet1");
+	driver.findElement(By.xpath("//input[@name='txtPassword']")).sendKeys("lab1");
+	driver.findElement(By.xpath("//input[@name='Submit']")).click();
+	
+	// 
+	if(driver.getTitle().equals(OrangeHRM - New Level of HR Management))
+	{
+		System.out.println("Pass");
+		else
+			System.out.println("Fail");
+	}
+	}
+}
